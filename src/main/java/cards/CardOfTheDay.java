e2a964fc916d097608baf4ea99d43c724e6652b4
lavendwein <pollinap23@yandex.ru>
2022-12-04 20:39:14 +0500

    horoscope

--- src/main/java/cards/CardOfTheDay.java
+++ src/main/java/cards/CardOfTheDay.java
@@ -1,5 +1,9 @@
 package cards;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import random.Random;
 
 import java.io.*;
@@ -19,12 +23,21 @@ public class CardOfTheDay implements CardsDay {
         Properties property = new Properties();
 
         try {
-            File file = new File("src\\main\\resources\\CardsOfTheDay.properties");
+            File file = new File("src\\main\\resources\\NameOfCards.properties");
             property.load(new FileReader(file));
 
             Random i = new Random();
             int number = i.randomNumber();
-            return property.getProperty("prediction" + number);
+            String name = property.getProperty("prediction" + number);
+
+            Document document = Jsoup.connect("https://astrohelper.ru/gadaniya/taro/znachenie/"
+                            + name + "/")
+                    .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                    .referrer("http://www.google.com")
+                    .get();
+            Elements h2 = document.select("h2");
+            String predicton = h2.get(3).text();
+            return predicton;
         } catch (IOException e) {
             System.err.println("Ошибка!");
         }
--- src/main/java/horoscope/HoroscopeOfTheDay.java
+++ src/main/java/horoscope/HoroscopeOfTheDay.java
@@ -2,10 +2,8 @@ package horoscope;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.util.HashMap;
 
 public class HoroscopeOfTheDay {
